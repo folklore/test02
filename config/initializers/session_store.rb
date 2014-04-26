bb67cfd653ec38703a949021295dfc3a5da83af9
Stanislav R. Gordanov <stanislav@gordanov.ru>
2014-04-26 21:28:01 +0600

    deploy

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Vladimir::Application.config.session_store :redis_store
+Vladimir::Application.config.session_store :redis_store,
+                                            'redis://rediscloud:M44rEgcHiIL1qmVw@pub-redis-19464.us-east-1-2.1.ec2.garantiadata.com:19464',
+                                            { expires_in: 90.minutes }
